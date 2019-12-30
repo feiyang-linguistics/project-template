98c2a8c80ce920ea2b5da207b14789b13dae3139
liao <liao961120@gmail.com>
2019-12-30 09:51:58 +0800

    update

--- src/compile_report.R
+++ src/compile_report.R
@@ -2,5 +2,5 @@
 rmarkdown::render("report/poster.Rmd")
 rmarkdown::render("report/report.Rmd")
 
-pagedown::chrome_print("report/poster.html")
-pagedown::chrome_print("report/report.html")
+pagedown::chrome_print("report/poster.html", output = "../G25_poster.pdf")
+pagedown::chrome_print("report/report.html", output = "../G25_report.pdf")
